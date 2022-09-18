a25727ac162351f8a2ac958f9527dd90cade6ad3
Alvin <alvin.tol@hotmail.com>
2022-09-17 19:54:02 -0600

    returns fixed rates response into dictionary

--- historicalRates.py
+++ historicalRates.py
@@ -1,4 +1,11 @@
 from requests import get
 from functools import reduce
 from csv import writer
+import json
 
+fixedResponse = get(
+    'https://www.ratehub.ca/api/charts?callback=rh.Chart.jsonpDispatch&id=Chart_zAHkl&series=best.5y-variable')
+
+fixedData = json.loads(fixedResponse.text[23:-2])
+
+print(fixedData)

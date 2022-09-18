02e5eb5a50ab7834079d4861102c2a23904e632c
Alvin <alvin.tol@hotmail.com>
2022-09-17 23:19:07 -0600

    corrects year and month conditionals for outdated data

--- historicalRates.py
+++ historicalRates.py
@@ -20,20 +20,22 @@ variableRate = variableData['best-5y-variable']
 
 rates = {}
 
-# with open('rates.csv', 'w') as csv_file:
-#     csv_writer = writer(csv_file)
-#     headers = ['Date', 'Fixed', 'Variable']
-#     csv_writer.writerow(headers)
+with open('rates.csv', 'w') as csv_file:
+    csv_writer = writer(csv_file)
+    headers = ['Date', 'Fixed', 'Variable']
+    csv_writer.writerow(headers)
 
 for fixDate in fixedDates:
     fixedIndex = fixedDates.index(fixDate)
-    convertFixDate = datetime.fromtimestamp(fixDate/1000).strftime('%m')
+    digitMonth = datetime.fromtimestamp(fixDate/1000).strftime('%m')
     year = datetime.fromtimestamp(fixDate/1000).strftime('%Y')
     month = datetime.fromtimestamp(fixDate/1000).strftime('%B')
 
-    if int(year) >= 2006 and int(convertFixDate) > 3:
+    if int(year) < 2006:
         continue
-    elif year not in rates:
+    elif int(year) == 2006 and int(digitMonth) <= 3:
+        continue
+    if year not in rates:
         rates[year] = {month: {'fixed': fixedRate[fixedIndex - 1]}}
     elif month not in rates[year]:
         rates[year][month] = {'fixed': fixedRate[fixedIndex - 1]}
@@ -41,7 +43,8 @@ for fixDate in fixedDates:
         rates[year][month]['fixed'] = fixedRate[fixedIndex - 1]
     else:
         rates[year][month]['fixed'] = (
-            rates[convertFixDate]['fixed'] + fixedRate[fixedIndex - 1]) / 2
+            rates[year][month]['fixed'] + fixedRate[fixedIndex - 1]) / 2
+
 for varDate in variableDates:
     varIndex = variableDates.index(varDate)
     convertVarDate = datetime.fromtimestamp(varDate/1000).strftime('%Y-%m')
@@ -58,10 +61,5 @@ for varDate in variableDates:
         rates[year][month]['variable'] = (
             rates[year][month]['variable'] + variableRate[varIndex - 1]) / 2
 
-print(rates)
-# for date in rates:
-#     try:
-#         csv_writer.writerow(
-#             [date, rates[date]['fixed'], rates[date]['variable']])
-#     except:
-#         csv_writer.writerow([date, rates[date]['fixed']])
+for year in rates:
+    print(year)

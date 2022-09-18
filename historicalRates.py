f913be2740251526ff0d09a21df370c010e694db
Alvin <alvin.tol@hotmail.com>
2022-09-17 19:56:41 -0600

    returns variable rates response into dictionary

--- historicalRates.py
+++ historicalRates.py
@@ -5,7 +5,11 @@ import json
 
 fixedResponse = get(
     'https://www.ratehub.ca/api/charts?callback=rh.Chart.jsonpDispatch&id=Chart_zAHkl&series=best.5y-variable')
+variableResponse = get(
+    'https://www.ratehub.ca/api/charts?callback=rh.Chart.jsonpDispatch&id=Chart_zAHkl&series=best.5y-variable')
 
 fixedData = json.loads(fixedResponse.text[23:-2])
+variableData = json.loads(variableResponse.text[23:-2])
 
-print(fixedData)
+# print(fixedData)
+print(variableData)

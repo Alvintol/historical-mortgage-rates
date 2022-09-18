b7b48476de52d44c9cfc3a949e4624e3634cd0f7
Alvin <alvin.tol@hotmail.com>
2022-09-17 20:00:11 -0600

    isolates date lists out of data

--- historicalRates.py
+++ historicalRates.py
@@ -4,12 +4,14 @@ from csv import writer
 import json
 
 fixedResponse = get(
-    'https://www.ratehub.ca/api/charts?callback=rh.Chart.jsonpDispatch&id=Chart_zAHkl&series=best.5y-variable')
+    'https://www.ratehub.ca/api/charts?callback=rh.Chart.jsonpDispatch&id=Chart_9EXxi&series=boc.5y-fixed-posted')
 variableResponse = get(
     'https://www.ratehub.ca/api/charts?callback=rh.Chart.jsonpDispatch&id=Chart_zAHkl&series=best.5y-variable')
 
 fixedData = json.loads(fixedResponse.text[23:-2])
 variableData = json.loads(variableResponse.text[23:-2])
 
-# print(fixedData)
-print(variableData)
+fixedDates = fixedData['date']
+variableDates = variableData['date']
+
+
